e420006679cf91208d6e4689a91532933c9611ac
Alex <2720-320@mail.ru>
2020-09-30 14:24:03 +0300

    done

--- src/com/calc/Main.java
+++ src/com/calc/Main.java
@@ -9,8 +9,5 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	 new Calculator().start();
-
-
-
     }
 }
--- src/com/calc/util/ServiceMessage.java
+++ src/com/calc/util/ServiceMessage.java
@@ -3,7 +3,6 @@ package com.calc.util;
 public enum ServiceMessage {
 
     HELLO_MESSAGE("Hi! I'm SuperCalc! What do you want to calculate?"),
-    INSTRUCTION_MESSAGE("Bla bla bla"),
     WRONG_INPUT("Wrong input! Please try again."),
     WRONG_NUMBER("Wrong number! Please try again.");
 
